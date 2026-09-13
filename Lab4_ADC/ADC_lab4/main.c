848d356db2ba635d62f4b1c84dff263d9a2658bb
Richard Lin <rlin113@aucklanduni.ac.nz>
2026-09-14 00:07:37 +1200

    Finished Q4.2

--- Lab4_ADC/ADC_lab4/main.c
+++ Lab4_ADC/ADC_lab4/main.c
@@ -4,13 +4,21 @@
  * Created: 13/09/2026 6:29:48 pm
  * Author : GGPC
  */ 
-
-#include <avr/io.h>
-#include "adc.h"
 #include "common.h"
+#include <avr/io.h>
+#include <util/delay.h>
+#include "adc.h"
 
 int main(void){
 	adc_init();
-	while(1){};
+	
+
+
+	
+	while(1){
+		
+		 adc_read(2);
+		_delay_ms(1);		
+		}
 }
 
